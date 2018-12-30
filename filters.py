c356d11af01138d26d7dc7551310933b2e4a3480
Inbar <inbar.gera@campus.technion.ac.il>
2018-12-30 20:04:07 +0100

    Part 2 + part3.1

--- filters.py
+++ filters.py
@@ -1,5 +1,6 @@
 from numba import cuda
 from numba import njit
+import numpy as np
 
 
 def convolution_gpu(kernel, image):
@@ -17,7 +18,6 @@ def convolution_gpu(kernel, image):
     '''
     raise NotImplementedError("To be implemented")
 
-
 @njit
 def convolution_numba(kernel, image):
     '''Convolve using numba
@@ -32,8 +32,44 @@ def convolution_numba(kernel, image):
     ------
     An numpy array of same shape as image
     '''
-    raise NotImplementedError("To be implemented")
+    kernel_size = (kernel.shape[0], kernel.shape[1])
+    new_kernel = np.zeros(kernel_size)
 
+    x_middle = (int)(kernel.shape[0] / 2 - (kernel.shape[0] / 2) % 1)  # got lazy to include math lib, this is simply floor
+    y_middle = (int)(kernel.shape[1] / 2 - (kernel.shape[1] / 2) % 1)
+
+    # first fliping the kernel martix
+    for x in range(0, kernel.shape[0]):
+        x_diff = abs(x - x_middle)*2
+        if (x > x_middle):
+            new_x = x - x_diff
+        else:
+            new_x = x + x_diff
+        for y in range(0, kernel.shape[1]):
+            y_diff = abs(y - y_middle)*2
+            if (y > y_middle):
+                new_y = y - y_diff
+            else:
+                new_y = y + y_diff
+            new_kernel[x][y] = kernel[new_x][new_y]
+
+    kernel = new_kernel # wrote the latter code with kernel, didnt want to change it
+
+    rows = image.shape[0]
+    cols = image.shape[1]
+    size = (rows, cols)
+    res = np.zeros(size)
+    for i in range(0, rows):
+        for j in range(0, cols):
+            temp = 0
+            for x in range(0, kernel.shape[0]):
+                for y in range(0, kernel.shape[1]):
+                    a = i + x - x_middle
+                    b = j + y - y_middle
+                    if(a < rows and a >= 0 and b < cols and b >= 0):
+                            temp += image[a][b] * kernel[x][y]
+            res[i][j] = temp
+    return res
 
 @cuda.jit
 def convolution_kernel():
--- my_queue.py
+++ my_queue.py
@@ -1,32 +1,51 @@
 from multiprocessing import Lock, Pipe
 
+# for debugging
+from multiprocessing import Process
+import time
 
 class MyQueue(object):
-
     def __init__(self):
-        ''' Initialize MyQueue and it's members.
-        '''
-
-        self.recv_end, self.send_end = Pipe()
-        self.lock = Lock()
-        raise NotImplementedError("To be implemented")
+        self.input_end, self.output_end = Pipe(False)
+        self.readLock = Lock()
+        # self.writeLock = Lock()
 
     def put(self, msg):
-        '''Put the given message in queue.
+        input, output = Pipe()
+        output.send(msg)
 
-        Parameters
-        ----------
-        msg : object
-            the message to put.
-        '''
-        raise NotImplementedError("To be implemented")
+        self.writeLock.acquire(True)
+        self.output_end.send(input)
+        self.writeLock.release()
 
     def get(self):
-        '''Get the next message from queue (FIFO)
-            
-        Return
-        ------
-        An object
-        '''
-        raise NotImplementedError("To be implemented")
+        next_pipe = self.input_end.recv()
+        return next_pipe.recv()
 
+
+# def threaded_function(queue, arg):
+#     for i in range(arg):
+#         queue.put("hello world")
+#     return 0
+#
+# def reader_function(queue):
+#     while True:
+#         print(queue.get())
+#
+# if __name__ == '__main__':
+#
+#     threadlilngs = []
+#     q = MyQueue()
+#     threads_amount = 50
+#     times_each_thread_prints = 1001
+#
+#     for _ in range(0, threads_amount):
+#         threadlilngs.append(Process(target=threaded_function, args=(q, times_each_thread_prints)))
+#
+#     for i in range(0, threads_amount):
+#         threadlilngs[i].start()
+#     reader = Process(target=reader_function, args=(q,))
+#     reader.start()
+#     for i in range(0, threads_amount):
+#         threadlilngs[i].join()
+#     print("finish joining")
